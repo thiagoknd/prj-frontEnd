2fd3415a70ace5aab7b7dbd1b86ee37a448e04b9
thiagoknd <thiagoarouchedev@hotmail.com>
2022-12-10 20:38:05 -0300

    alterações no front end e criação de uma fake api

---
+++ prj-vendas/fake-api/produto.js
@@ -0,0 +1,12 @@
+[
+    {"nome:":"Tenis Adidas","img:":"https://github.com/thiagoknd/prj-frontEnd/fake-api/img/tenisAdidas.jpg","valor":800},
+    {"nome:":"Tenis Fila","img:":"https://github.com/thiagoknd/prj-frontEnd/fake-api/img/tenisFila.jpg","valor":700},
+    {"nome:":"Tenis Jordan","img:":"https://github.com/thiagoknd/prj-frontEnd/fake-api/img/tenisJordan.jpg","valor":1250},
+    {"nome:":"Tenis Mizuno","img:":"https://github.com/thiagoknd/prj-frontEnd/fake-api/img/tenisMizuno.jpg","valor":850},
+    {"nome:":"Tenis Nike","img:":"https://github.com/thiagoknd/prj-frontEnd/fake-api/img/tenisNike.jpg","valor":900},
+    {"nome:":"Tenis Olympikus","img:":"https://github.com/thiagoknd/prj-frontEnd/fake-api/img/tenisOlimp.jpg","valor":500},
+    {"nome:":"Tenis Puma","img:":"https://github.com/thiagoknd/prj-frontEnd/fake-api/img/tenisPuma.jpg","valor":450},
+    {"nome:":"Tenis Under","img:":"https://github.com/thiagoknd/prj-frontEnd/fake-api/img/TenisUnder.jpg","valor":320},
+    
+    
+]
