4ba5afc593a0aa8f6f46be732027feff342a33da
thiagoknd <thiagoarouchedev@hotmail.com>
2022-12-11 16:32:47 -0300

    alteração do index e do arquivo js da fake api

--- prj-vendas/fake-api/produto.js
+++ prj-vendas/fake-api/produto.js
@@ -1,12 +1,12 @@
 [
-    {"nome:":"Tenis Adidas","img:":"https://thiagoknd.github.io/prj-frontEnd/prj-vendas/fake-api/img/tenisAdidas.jpg","valor":800},
-    {"nome:":"Tenis Fila","img:":"https://thiagoknd.github.io/prj-frontEnd/prj-vendas/fake-api/img/tenisFila.jpg","valor":700},
-    {"nome:":"Tenis Jordan","img:":"https://thiagoknd.github.io/prj-frontEnd/prj-vendas/fake-api/img/tenisJordan.jpg","valor":1250},
-    {"nome:":"Tenis Mizuno","img:":"https://thiagoknd.github.io/prj-frontEnd/prj-vendas/fake-api/img/tenisMizuno.jpg","valor":850},
-    {"nome:":"Tenis Nike","img:":"https://thiagoknd.github.io/prj-frontEnd/prj-vendas/fake-api/img/tenisNike.jpg","valor":900},
-    {"nome:":"Tenis Olympikus","img:":"https://thiagoknd.github.io/prj-frontEnd/prj-vendas/fake-api/img/tenisOlimp.jpg","valor":500},
-    {"nome:":"Tenis Puma","img:":"https://thiagoknd.github.io/prj-frontEnd/prj-vendas/fake-api/img/tenisPuma.jpg","valor":450},
-    {"nome:":"Tenis Under","img:":"https://thiagoknd.github.io/prj-frontEnd/prj-vendas/fake-api/img/tenisUnder.jpg","valor":320}
+    {"nome":"Tenis Adidas","img":"https://thiagoknd.github.io/prj-frontEnd/prj-vendas/fake-api/img/tenisAdidas.jpg","valor":800},
+    {"nome":"Tenis Fila","img":"https://thiagoknd.github.io/prj-frontEnd/prj-vendas/fake-api/img/tenisFila.jpg","valor":700},
+    {"nome":"Tenis Jordan","img":"https://thiagoknd.github.io/prj-frontEnd/prj-vendas/fake-api/img/tenisJordan.jpg","valor":1250},
+    {"nome":"Tenis Mizuno","img":"https://thiagoknd.github.io/prj-frontEnd/prj-vendas/fake-api/img/tenisMizuno.jpg","valor":850},
+    {"nome":"Tenis Nike","img":"https://thiagoknd.github.io/prj-frontEnd/prj-vendas/fake-api/img/tenisNike.jpg","valor":900},
+    {"nome":"Tenis Olympikus","img":"https://thiagoknd.github.io/prj-frontEnd/prj-vendas/fake-api/img/tenisOlimp.jpg","valor":500},
+    {"nome":"Tenis Puma","img":"https://thiagoknd.github.io/prj-frontEnd/prj-vendas/fake-api/img/tenisPuma.jpg","valor":450},
+    {"nome":"Tenis Under","img":"https://thiagoknd.github.io/prj-frontEnd/prj-vendas/fake-api/img/tenisUnder.jpg","valor":320}
     
     
 ]
